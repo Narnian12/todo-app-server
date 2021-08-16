3d2a059c14c2294d82d83573e0bbaca70f5891a8
Peter Sun <pitaah@umich.edu>
2021-08-16 10:52:21 -0400

    - Initial creation of server for todo app

---
+++ README.md
@@ -0,0 +1,7 @@
+# TODO App Server
+
+Server-side implementation of todo app using Apollo Server. Includes adding and deleting todo items.
+
+## Instructions
+* `npm install`
+* `npm start`
---
+++ src/index.js
@@ -0,0 +1,55 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const fs = require('fs');
+
+var todoList = [];
+fs.readFile('./src/data.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  let parsedData = JSON.parse(data);
+  todoList = parsedData;
+});
+
+// Locally adds todo and writes todo into JSON file
+function addWriteTodo(todo) {
+  todoList.list.push(todo);
+  writeTodo();
+}
+
+function deleteTodoHelper(id) {
+  todoList.list = todoList.list.filter(todo => todo.id !== id);
+  console.log(todoList);
+  writeTodo();
+}
+
+function writeTodo() {
+  fs.writeFile('./src/data.json', JSON.stringify(todoList), err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+}
+
+const resolvers = {
+  Query: {
+    getTodoList: () => todoList.list
+  },
+  Mutation: {
+    addTodo: (_, { todo }) => {
+      addWriteTodo(todo);
+      return todo;
+    },
+    deleteTodo: (_, { id }) => {
+      deleteTodoHelper(id);
+      return id;
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
